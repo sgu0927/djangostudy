ea359ba6c31be77b6b7c2b4230711d35ff857a00
sgu0927 <mylazysong@naver.com>
2020-09-08 13:37:11 +0900

    upload project 1,2

---
+++ community_test/board/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.http import Http404
+from django.core.paginator import Paginator
+from .models import Board
+from .forms import BoardForm
+from user.models import User
+from tag.models import Tag
+
+# Create your views here.
+def board_detail(request,pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404('게시글을 찾을 수 없습니다.')
+    return render(request,'board_detail.html',{'board':board})
+def board_write(request):
+    if not request.session.get('user'):
+        return redirect('/user/login')
+
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            user_id = request.session.get('user')
+            user = User.objects.get(pk=user_id)
+
+            board = Board()
+            board.title = form.cleaned_data['title']
+            board.contents = form.cleaned_data['contents']
+            board.writer = user
+            board.save()
+
+            tags = form.cleaned_data['tags'].split(',')
+            
+            for tag in tags:
+                if not tag: continue
+
+                _tag, _ = Tag.objects.get_or_create(name=tag)
+                board.tags.add(_tag)
+
+            return redirect('/board/list')
+    else:
+        form = BoardForm()
+    return render(request,'board_write.html',{'form': form})
+
+def board_list(request):
+    all_boards = Board.objects.all().order_by('-id')
+    page = int(request.GET.get('p',1))
+    paginator = Paginator(all_boards,2)
+    boards = paginator.get_page(page)
+
+    return render(request,'board_list.html', {'boards':boards})
